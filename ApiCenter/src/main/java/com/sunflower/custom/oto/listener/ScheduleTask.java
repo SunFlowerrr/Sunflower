f0680e77dd06bd344a84aeba5b1affd70b620629
Lin <linzhongtai@gengee.cn>
2017-08-23 13:05:55 +0800

    【Sunflower】定时器test

--- ApiCenter/src/main/java/com/sunflower/custom/oto/listener/ScheduleTask.java
+++ ApiCenter/src/main/java/com/sunflower/custom/oto/listener/ScheduleTask.java
@@ -10,8 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleTask {
     private final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-
-    @Scheduled(fixedDelay = 2000)
+    //    cron表达式：
+    //    每隔5秒执行一次：*/5 * * * * ?
+    //    每隔1分钟执行一次：0 1 * * * ?
+    //    每天23点执行一次：0 0 23 * * ?
+    //    每天凌晨1点执行一次：0 0 1 * * ?
+    //    每月1号凌晨1点执行一次：0 0 1 1 * ?
+    //    每月最后一天23点执行一次：0 0 23 L * ?
+    //    每周星期天凌晨1点实行一次：0 0 1 ? * L
+    @Scheduled(cron = "*/5 * * * * ?")
     public void saySomething() {
         LOGGER.info("定时任务开始");
         System.out.println("123456");
